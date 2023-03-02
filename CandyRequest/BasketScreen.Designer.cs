5343626e938d321f58a7daa7a758cb65d45d8ef0
Sarkhanas <daniilzon@mail.ru>
2023-03-02 07:50:17 +0300

    BasketScreen started

--- CandyRequest/BasketScreen.Designer.cs
+++ CandyRequest/BasketScreen.Designer.cs
@@ -29,19 +29,92 @@ namespace CandyRequest
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.saveBillBtn = new System.Windows.Forms.Button();
+            this.backBtn = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(575, 235);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.Location = new System.Drawing.Point(506, 331);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(82, 23);
+            this.submitBtn.TabIndex = 1;
+            this.submitBtn.Text = "Подтвердить";
+            this.submitBtn.UseVisualStyleBackColor = true;
+            this.submitBtn.Click += new System.EventHandler(this.submitBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(13, 255);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearBtn.TabIndex = 2;
+            this.clearBtn.Text = "Очистить";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            // 
+            // saveBillBtn
+            // 
+            this.saveBillBtn.Location = new System.Drawing.Point(12, 284);
+            this.saveBillBtn.Name = "saveBillBtn";
+            this.saveBillBtn.Size = new System.Drawing.Size(91, 23);
+            this.saveBillBtn.TabIndex = 3;
+            this.saveBillBtn.Text = "Получить чек";
+            this.saveBillBtn.UseVisualStyleBackColor = true;
+            // 
+            // backBtn
+            // 
+            this.backBtn.Location = new System.Drawing.Point(13, 331);
+            this.backBtn.Name = "backBtn";
+            this.backBtn.Size = new System.Drawing.Size(75, 23);
+            this.backBtn.TabIndex = 4;
+            this.backBtn.Text = "Назад";
+            this.backBtn.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // BasketScreen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.backBtn);
+            this.Controls.Add(this.saveBillBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.submitBtn);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "BasketScreen";
             this.Text = "CandyRequest";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.BasketScreen_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button submitBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button saveBillBtn;
+        private System.Windows.Forms.Button backBtn;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- CandyRequest/BasketScreen.cs
+++ CandyRequest/BasketScreen.cs
@@ -12,9 +12,33 @@ namespace CandyRequest
 {
     public partial class BasketScreen : Form
     {
+        public int i = 0;
+
         public BasketScreen()
         {
             InitializeComponent();
+        }
+
+        private void BasketScreen_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void submitBtn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ожидайте, администратор проверяет ваш заказ!");
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            i++;
+            if (i == timer1.Interval)
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("Ваш заказ одобрен");
+            }
+                
         }
     }
 }
